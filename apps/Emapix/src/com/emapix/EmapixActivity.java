2d645a03f9e80c13f225ab46a5879d978fe1600d
Alex Dementsov <dexity@gmail.com>
2012-02-14 01:28:30 -0800

    Added first files for Android app

---
+++ apps/Emapix/src/com/emapix/EmapixActivity.java
@@ -0,0 +1,15 @@
+package com.emapix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmapixActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
