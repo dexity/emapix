fb3ecfe4b605835650d8ddd60539a89c2b274e62
Alex Dementsov <dexity@gmail.com>
2012-02-17 10:07:52 -0800

    Implemented simple Google map display.

--- apps/Emapix/src/com/emapix/EmapixActivity.java
+++ apps/Emapix/src/com/emapix/EmapixActivity.java
@@ -2,14 +2,23 @@ package com.emapix;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MapActivity;
 
-public class EmapixActivity extends Activity
-{
+public class EmapixActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);        
     }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }   
+    
 }
