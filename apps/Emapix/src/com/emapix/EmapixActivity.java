4e0ec532cebbcd22d47b993dcb022c4f0b99d18f
Alex Dementsov <dexity@gmail.com>
2012-02-22 05:58:47 -0800

    Implemented long press feature. It moves to location where the long press is performed.

--- apps/Emapix/src/com/emapix/EmapixActivity.java
+++ apps/Emapix/src/com/emapix/EmapixActivity.java
@@ -1,19 +1,40 @@
 package com.emapix;
 
-import android.app.Activity;
 import android.os.Bundle;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MapActivity;
 
 public class EmapixActivity extends MapActivity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        MapView mapView = (MapView) findViewById(R.id.mapview);
-        mapView.setBuiltInZoomControls(true);        
+        EmapixMapView mView = (EmapixMapView) findViewById(R.id.mapview);
+        mView.setBuiltInZoomControls(true);   
+        
+        GeoPoint point	= new GeoPoint(32818062,-117269440);
+		MapController mController = mView.getController();
+		mController.setZoom(14);
+		mController.animateTo(point);
+		
+        mView.setOnLongpressListener(new EmapixMapView.OnLongpressListener() {
+	        public void onLongpress(final MapView view, final GeoPoint longpressLocation) {
+	            runOnUiThread(new Runnable() {
+		            public void run() {
+		                MapController mController = view.getController();
+		                mController.setZoom(14);
+		                mController.animateTo(longpressLocation);
+		            }
+		        });
+	        }
+        });
+
     }
     
     @Override
