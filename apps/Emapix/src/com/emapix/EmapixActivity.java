98477a6d7e141e2e5658b03fb306c9413a7b19ca
Alex Dementsov <dexity@gmail.com>
2012-02-23 05:15:09 -0800

    Marker gets set when user makes the long press

--- apps/Emapix/src/com/emapix/EmapixActivity.java
+++ apps/Emapix/src/com/emapix/EmapixActivity.java
@@ -21,126 +21,25 @@ import com.google.android.maps.OverlayItem;
 public class EmapixActivity extends MapActivity {
 	
 	// Move to a separate class?
-	public class MarkerItemizedOverlay extends ItemizedOverlay<OverlayItem> {
-		
-		private OverlayItem mOverlay;
-		Context mContext;
-
-		public MarkerItemizedOverlay(Drawable defaultMarker) {
-			//super(boundCenterBottom(defaultMarker));
-			super(defaultMarker);
-			//mContext = context;
-			populate();	// Important
-		}
-		
-		public void setOverlay(OverlayItem overlay) {
-		    mOverlay = overlay;
-		    populate();
-		}	
-
-		@Override
-		protected boolean onTap(int index) {
-			// No action
-			return true;
-		}	
-		
-		@Override
-		protected OverlayItem createItem(int i) {
-			return mOverlay;
-		}
-		
-		@Override
-		public int size() {
-			if (mOverlay != null)
-				return 1;
-			return 0;
-		}
-	}	
-	
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        EmapixMapView mView = (EmapixMapView) findViewById(R.id.mapview);
-        mView.setBuiltInZoomControls(true);   
-        
-//        List<Overlay> mOverlays = mView.getOverlays();
-//        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
-//        MarkerItemizedOverlay itemizedoverlay = new MarkerItemizedOverlay(drawable);        
-//        mOverlays.add(itemizedoverlay);
-        
-        // Initial position
-        GeoPoint point	= new GeoPoint(32818062,-117269440);
-		MapController mController = mView.getController();
-		mController.setZoom(14);
-		mController.animateTo(point);
-		
-        mView.setOnLongpressListener(new EmapixMapView.OnLongpressListener() {
-	        public void onLongpress(final MapView view, final GeoPoint lpPoint) {
-	            runOnUiThread(new Runnable() {
-		            public void run() {
-		            	// Add image
-		                List<Overlay> mOverlays = view.getOverlays();
-		            	
-		                Log.i("OVERLAYS", mOverlays.toString());
-		                
-		                Drawable drawable = view.getResources().getDrawable(R.drawable.androidmarker);
-		                MarkerItemizedOverlay itemizedoverlay = new MarkerItemizedOverlay(drawable);//, view.getContext());        
-		                
-		            	OverlayItem overlayitem = new OverlayItem(lpPoint, "", "");
-		            	itemizedoverlay.setOverlay(overlayitem);
-		            	
-		                mOverlays.add(itemizedoverlay);		            	
-		            	
-		                
-//		            	List<Overlay> overlays = view.getOverlays();
-//		            	// We added one overlay before
-//		            	MarkerItemizedOverlay overlay	= (MarkerItemizedOverlay)overlays.get(0);
-//		            	OverlayItem overlayitem = new OverlayItem(lpPoint, "", "");
-//		            	overlay.setOverlay(overlayitem);
-		            	
-		                
-//		                MapController mController = view.getController();
-//		                mController.setZoom(14);
-//		                mController.animateTo(lpPoint);
-		            }
-		        });
-	        }
-        });
-
-    }
-    
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }      
-}
-
-/*
-
-	
 	public class MarkerItemizedOverlay extends ItemizedOverlay {
 		
 		private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 		Context mContext;
 
-		public MarkerItemizedOverlay(Drawable defaultMarker) {
-			//super(boundCenterBottom(defaultMarker));
-			super(defaultMarker);
-			//mContext = context;
+		public MarkerItemizedOverlay(Drawable defaultMarker, Context context) {
+			super(boundCenterBottom(defaultMarker));
+			mContext = context;
+			populate();	// Important
 		}
 		
-		public void setOverlay(OverlayItem overlay) {
+		public void addOverlay(OverlayItem overlay) {
+			mOverlays.clear();
 			mOverlays.add(overlay);
 		    populate();
 		}	
 
 		@Override
 		protected boolean onTap(int index) {
-			// No action
 			return true;
 		}	
 		
@@ -155,4 +54,50 @@ public class EmapixActivity extends MapActivity {
 		}
 	}		
 	
- */
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        EmapixMapView mView = (EmapixMapView) findViewById(R.id.mapview);
+        mView.setBuiltInZoomControls(true);   
+        
+        // Initial position
+        GeoPoint point	= new GeoPoint(32818062,-117269440);
+		MapController mController = mView.getController();
+		mController.setZoom(14);
+		mController.animateTo(point);
+
+		List<Overlay> mOverlays = mView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        MarkerItemizedOverlay itemizedoverlay = new MarkerItemizedOverlay(drawable, this);        
+        mOverlays.add(itemizedoverlay);
+        		
+        mView.setOnLongpressListener(new EmapixMapView.OnLongpressListener() {
+	        public void onLongpress(final MapView view, final GeoPoint lpPoint) {
+	            runOnUiThread(new Runnable() {
+		            public void run() {
+		            	// Add image
+	                
+		            	List<Overlay> overlays = view.getOverlays();
+		            	// We added one overlay before
+		            	MarkerItemizedOverlay overlay	= (MarkerItemizedOverlay)overlays.get(0);
+		            	OverlayItem overlayitem = new OverlayItem(lpPoint, "", "");
+		            	overlay.addOverlay(overlayitem);
+
+		            	view.invalidate();
+		            }
+		        });
+	        }
+        });
+
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }      
+}
+
