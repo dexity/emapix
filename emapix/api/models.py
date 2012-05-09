83b5200eb3193466d3fe1e25279259aebd529d6d
Alex Dementsov <dexity@gmail.com>
2012-05-08 17:47:50 -0700

    Implemented PhotoRequest model.

--- emapix/api/models.py
+++ emapix/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import IntegerField, CharField
 
-
+class PhotoRequest(models.Model):
+    lat     = IntegerField(default=0)
+    lon     = IntegerField(default=0)
+    submitted_date  = CharField(max_length=16)  # timestamp
+    resource    = CharField(max_length=16)
+    
+    def __unicode__(self):
+        return "(%s, %s)" % (self.lat, self.lon)    
