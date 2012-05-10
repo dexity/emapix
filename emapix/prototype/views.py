0508e6af477512f8e371d167cd801a73ba260b8b
Alex Dementsov <dexity@gmail.com>
2012-05-10 10:13:34 -0700

    Added logging for testing

--- emapix/prototype/views.py
+++ emapix/prototype/views.py
@@ -6,12 +6,15 @@ from django.core.context_processors import csrf
 from emapix.utils import handle_uploaded_file
 from forms import UploadFileForm
 
+logger = Logger.get("emapix.prototype.views")
+
 def index(request):
     c = {}
-    c.update(csrf(request))
+    #c.update(csrf(request))    
         
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
+        logger.debug(str(form))
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
             return HttpResponseRedirect('/')
