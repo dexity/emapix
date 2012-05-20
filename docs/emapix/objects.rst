3295038e2af89414f046bd6f83c82f59321e5e40
Alex Dementsov <dexity@gmail.com>
2012-05-19 21:49:26 -0700

    Started work on the first release

--- docs/emapix/objects.rst
+++ docs/emapix/objects.rst
@@ -3,3 +3,31 @@
 Objects
 =======
 
+Request
+Comment
+Photo
+    id
+    from
+    name
+    picture     # The thumbnail-sized source of the photo
+    source      # The source image of the photo
+    request
+    height
+    width
+    images      # list of images
+        height
+        width
+        source
+        
+    link        # A link to the photo on Emapix 
+    location
+    created_time
+    updated_time
+    
+    comments    # list of comments
+    
+User
+    
+    
+
+
--- emapix/api/models.py
+++ emapix/api/models.py
@@ -10,3 +10,35 @@ class PhotoRequest(models.Model):
     
     def __unicode__(self):
         return "(%s, %s)" % (self.lat, self.lon)    
+
+
+#class Request(models.Model):
+#    pass
+#
+#
+#class Photo(models.Model):
+#    user
+#    name
+#    picture     # The thumbnail-sized source of the photo
+#    source      # The source image of the photo
+#    request
+#    height
+#    width
+#    images      
+#    location
+#    created_time
+#    updated_time
+#
+#
+#class PhotoRequest(models.Model):
+#    pass
+#
+#
+#class Comment(models.Model):
+#    photo   = ""
+#    pass
+#
+#
+#class User(models.Model):
+#    pass
+
