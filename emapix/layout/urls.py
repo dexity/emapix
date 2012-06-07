5315a4988f0421fa70e4e0c59f51a91e3646ff0c
Alex Dementsov <dexity@gmail.com>
2012-06-06 21:45:01 -0700

    Added stub files for other layouts

--- emapix/layout/urls.py
+++ emapix/layout/urls.py
@@ -3,4 +3,13 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('emapix.layout.views',
     url(r'^$', 'index'),
     url(r'^requests$', 'requests'),
+    url(r'^request$', 'request'),
+    url(r'^requests2$', 'requests2'),
+    url(r'^photos$', 'photos'),
+    url(r'^search$', 'search'),
+    url(r'^search2$', 'search2'),
+    url(r'^submit$', 'submit'),
+    url(r'^submit2$', 'submit2'),
+    url(r'^submit3$', 'submit3'),
+    url(r'^make$', 'make'),
 )
--- emapix/layout/views.py
+++ emapix/layout/views.py
@@ -7,6 +7,34 @@ logger = Logger.get("emapix.layout.views")
 def index(request):
     return render_to_response('index.html')
 
-
 def requests(request):
     return render_to_response('requests.html')
+
+def request(request):
+    return render_to_response('request.html')
+
+def requests2(request):
+    return render_to_response('requests2.html')
+
+def photos(request):
+    return render_to_response('photos.html')
+
+def search(request):
+    return render_to_response('search.html')
+
+def search2(request):
+    return render_to_response('search2.html')
+
+def submit(request):
+    return render_to_response('submit.html')
+
+def submit2(request):
+    return render_to_response('submit2.html')
+
+def submit3(request):
+    return render_to_response('submit3.html')
+
+def make(request):
+    return render_to_response('make.html')
+
+
