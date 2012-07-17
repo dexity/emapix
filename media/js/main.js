800b1e1a2e0f7f3db1ff2fc9961c37a593ba550c
Alex Dementsov <dexity@gmail.com>
2012-07-17 08:50:35 -0700

    Some more changes

--- media/js/main.js
+++ media/js/main.js
@@ -1,4 +1,22 @@
 
+
+var month_days = {
+    "JAN":  31,
+    "FEB":  29,
+    "MAR":  31,
+    "APR":  30,
+    "MAY":  31,
+    "JUN":  30,
+    "JUL":  31,
+    "AUG":  31,
+    "SEP":  30,
+    "OCT":  31,
+    "NOV":  30,
+    "DEC":  31,
+}
+
+
+
 var map;
 var infoWindow;
 var markersArray	= [];
