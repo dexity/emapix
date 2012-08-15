15b875bcb75b4628380a18e702aeadd652f61f0f
Alex Dementsov <dexity@gmail.com>
2012-08-15 09:31:19 -0700

    Implemented ts2utc() method

--- testing/tests/utils/test_utils.py
+++ testing/tests/utils/test_utils.py
@@ -1,10 +1,20 @@
 import unittest
-from emapix.utils.utils import ts2h
+import time
+
+from emapix.utils.utils import ts2h, ts2utc
+from fixtures import *
 
 class UtilsTest(unittest.TestCase):
 
     def setUp(self):
         pass
     
+    
     def test_ts2h(self):
+        
         print "Hello world!"
+        
+        
+    def test_ts2utc(self):
+        self.assertEqual(ts2utc(TS), TS_UTC)
+        
