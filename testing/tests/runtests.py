f54979f66c40a1dc4aad9ffb1ab1c5be1143fde1
Alex Dementsov <dexity@gmail.com>
2012-08-15 07:24:30 -0700

    Added test files to test utils

---
+++ testing/tests/runtests.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# See Capture plugin
+# http://somethingaboutorange.com/mrl/projects/nose/1.0.0/plugins/capture.html
+
+import os
+f = os.popen("nosetests -s --verbosity=2")
+print f.read()
---
+++ testing/tests/utils/test_utils.py
@@ -0,0 +1,10 @@
+import unittest
+from emapix.utils.utils import ts2h
+
+class UtilsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def test_ts2h(self):
+        print "Hello world!"
