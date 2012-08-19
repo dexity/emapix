d46eb3f6640271c5e5ea00b62348793b77216ac4
Alex Dementsov <dexity@gmail.com>
2012-08-19 10:51:24 -0700

    Added temp script which migrates lat,lon from request to location

---
+++ emapix/scripts/migrations/refactor_location.py
@@ -0,0 +1,21 @@
+import os, sys
+
+sys.path.append("../../../")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'emapix.settings'
+
+from emapix.core.models import Request, Location
+
+def migrate():
+    reqs    = Request.objects.all()
+    for req in reqs:
+        l   = Location()
+        l.lat   = req.lat
+        l.lon   = req.lon
+        l.save()
+        req.location    = l
+        req.save()
+
+
+if __name__ == '__main__':
+    migrate()
+
